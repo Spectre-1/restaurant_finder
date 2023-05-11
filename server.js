8c739364aa3bfadd2fcc0975778d8b5c44a29d81
Spectre <pgudijan@uci.edu>
2023-05-10 17:11:21 -0700

    initial front end

--- server.js
+++ server.js
@@ -5,3 +5,22 @@ const port = 3000;
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+
+const axios = require('axios');
+
+axios.get('https://api.yelp.com/v3/businesses/search', {
+  params: {
+    location: 'San Francisco',
+    term: 'food',
+    limit: 10
+  },
+  headers: {
+    Authorization: `Bearer ${YOUR_YELP_API_KEY}`
+  }
+})
+.then(response => {
+  console.log(response.data);
+})
+.catch(error => {
+  console.error(error);
+});
