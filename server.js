22f5ae327071783faa9a0bc1a74266124819ed57
Spectre <pgudijan@uci.edu>
2023-05-17 17:51:25 -0700

    Fixed API, JSON now displays on webpage

--- server.js
+++ server.js
@@ -1,31 +1,31 @@
-require('dotenv').config();
+require('dotenv').config({ path: './secret/secret.env' });
 const express = require('express');
 const app = express();
-const port = 3000;
-
-app.get('/', (req, res) => res.send('Hello World!'));
-
-app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
-
+const port = 3001;
 const axios = require('axios');
 
-const yelpApiKey = process.env.YELP_API_KEY;
-// Access the Yelp API key from the environment variable
-// Refer to info.env file for key
+app.get('/', (req, res) => {
+  const yelpApiKey = process.env.YELP_API_KEY;
 
-axios.get('https://api.yelp.com/v3/businesses/search', {
-  params: {
-    location: 'San Francisco',
-    term: 'food',
-    limit: 10
-  },
-  headers: {
-    Authorization: `Bearer ${yelpApiKey}` // Use the yelpApiKey variable
-  }
-})
-.then(response => {
-  console.log(response.data);
-})
-.catch(error => {
-  console.error(error);
+  axios.get('https://api.yelp.com/v3/transactions/delivery/search', {
+    params: {
+      location: 'San Francisco',
+      open_now: true, // this parameter only exists in the Transaction Search Endpoint and not in the Business Search Endpoint
+      limit: 10
+    },
+    headers: {
+      Authorization: `Bearer ${yelpApiKey}`
+    }
+  })
+  .then(response => {
+    // Convert JSON data to string for simple display
+    // You can modify this part depending on how you want to format the data
+    res.send(JSON.stringify(response.data, null, 2));
+  })
+  .catch(error => {
+    console.error(error);
+    res.status(500).send('An error occurred while retrieving data from Yelp API');
+  });
 });
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
